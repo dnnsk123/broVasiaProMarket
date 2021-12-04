3865dd756869f8870ecbde2852fc96052960999f
dnnsk123 <dennisok2021@gmail.com>
2021-12-04 19:59:40 +0200

    commit

--- js.js
+++ js.js
@@ -1,1 +1,51 @@
-DataViewcdsvbsvbsrdnucydstcvvgdxjhjdvtftdgx
+let item = document.getElementById('grid')
+ let xhr = new XMLHttpRequest
+let url = 'https://my-json-server.typicode.com/dnnsk123/broVasiaProMarket'
+xhr.open("GET", url + "/products/")
+xhr.responseType = 'json'
+xhr.send()
+xhr.onload = function() {
+    let products = xhr.response
+    console.log(products)
+    item.innerHTML = `
+    <div class = "item">
+        <h1>${products[0].name}</h1>
+        <img width = "180px" src='${products[0].photo_url}'>
+        <h1>${products[0].price}</h1>
+        <h1>${products[0].description}</h1>
+        </div>
+        <div class = "item">
+        <h1>${products[1].name}</h1>
+        <img width = "180px" src='${products[1].photo_url}'>
+        <h1>${products[1].price}</h1>
+        <h1>${products[1].description}</h1>
+        </div>
+        <div class = "item">
+        <h1>${products[2].name}</h1>
+        <img width = "180px" src='${products[2].photo_url}'>
+        <h1>${products[2].price}</h1>
+        <h1>${products[2].description}</h1>
+        </div>
+        <div class = "item">
+        <h1>${products[3].name}</h1>
+        <img width = "180px" src='${products[3].photo_url}'>
+        <h1>${products[3].price}</h1>
+        <h1>${products[3].description}</h1>
+        </div>
+        <div class = "item">
+        <h1>${products[4].name}</h1>
+        <img width = "180px" src='${products[4].photo_url}'>
+        <h1>${products[4].price}</h1>
+        <h1>${products[4].description}</h1>
+        </div>
+        <div class = "item">
+        <h1>${products[5].name}</h1>
+        <img width = "180px" src='${products[5].photo_url}'>
+        <h1>${products[5].price}</h1>
+        <h1>${products[5].description}</h1>
+     
+     
+    </div>
+    `
+
+}
